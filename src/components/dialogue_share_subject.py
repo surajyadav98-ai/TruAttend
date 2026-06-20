f89b57ce9421dda21fc38a70539e0586f91e1a00
surajyadav98-ai <sy9226093@gmail.com>
2026-06-20 23:25:58 +0530

    Update dialogue_share_subject.py

--- src/components/dialogue_share_subject.py
+++ src/components/dialogue_share_subject.py
@@ -3,7 +3,7 @@ import segno
 import io
 @st.dialog("Share class link")
 def share_subject_dialog(subject_name,subject_code):
-   app_domain = "TruAttend.streamlit.app"
+   app_domain = "TruAttend-main.streamlit.app"
    join_url = f"{app_domain}/?join-code={subject_code}"
    st.header
 
